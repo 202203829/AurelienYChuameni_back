1800a41ebcec0aee9e651229a9f5de2032e7a7b4
202203829 <202203829@alu.comillas.edu>
2025-04-13 16:18:03 +0200

    try

--- dynamic-frontend/dynamic/aurelienychuameni/app/page.js
+++ dynamic-frontend/dynamic/aurelienychuameni/app/page.js
@@ -1,9 +1,35 @@
 "use client";
 
+import Layout from "./componentes/Layout/Layout";
+import styles from './componentes/Layout/styles.module.css';
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/subastas?query=${encodeURIComponent(query.trim())}`);
+  };
+
   return (
-    <div>
-      <h1>Funciona en Vercel</h1>
-    </div>
+    <Layout>
+      <label className={styles.bigLabel} htmlFor="buscar">
+        Pon lo que buscas… y prepárate para la guerra.
+      </label>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="text"
+          placeholder="Buscar..."
+          name="buscar"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className={styles.input}
+        />
+        <button type="submit" className={styles.button}>Buscar</button>
+      </form>
+    </Layout>
   );
 }
